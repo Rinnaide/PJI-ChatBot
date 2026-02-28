5b82cc121564c831e7db1e22787098454ac3ff03
rinnaide <phencostarj@gmail.com>
2026-02-28 11:50:32 -0300

    home header troca de cor para f7f7f7

--- Frontend/chatbotProj/app/(tabs)/index.tsx
+++ Frontend/chatbotProj/app/(tabs)/index.tsx
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     padding: 20,
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
-    backgroundColor: '#f8f8f8',
+    backgroundColor: '#f7f7f7',
   },
   headerText: {
     fontSize: 20,
